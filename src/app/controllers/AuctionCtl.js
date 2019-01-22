113348d0661b71ef39e535242720bb34bdc29cd8
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2019-01-22 13:09:42 +0200

    CS-2049: removing GTM and loggy tracking/new server logging

--- src/app/controllers/AuctionCtl.js
+++ src/app/controllers/AuctionCtl.js
@@ -30,11 +30,6 @@ angular.module('auction').controller('AuctionController', [
     $rootScope.default_http_error_timeout = 500;
     $rootScope.http_error_timeout = $rootScope.default_http_error_timeout;
     $rootScope.browser_client_id = AuctionUtils.generateUUID();
-    $rootScope.$watch(function() {
-      return $cookies.logglytrackingsession;
-    }, function(newValue, oldValue) {
-      $rootScope.browser_session_id = $cookies.logglytrackingsession;
-    });
     window.onunload = function() {
       $log.info("Close window");
       if ($rootScope.changes) {
@@ -45,14 +40,11 @@ angular.module('auction').controller('AuctionController', [
       }
     };
     if (AuctionConfig.auction_doc_id.indexOf("_") > 0 ){
-      dataLayer.push({
-        "tenderId": AuctionConfig.auction_doc_id.split("_")[0],
-        "lotId": AuctionConfig.auction_doc_id.split("_")[1]
-      });
+      var doc_id_parts =  AuctionConfig.auction_doc_id.split("_");
+      $log.context["TENDER_ID"] = doc_id_parts[0];
+      $log.context["LOT_ID"] = doc_id_parts[1];
     } else {
-      dataLayer.push({
-        "tenderId": AuctionConfig.auction_doc_id
-      });
+      $log.context["TENDER_ID"] = AuctionConfig.auction_doc_id;
     }
     $log.info({
       message: "Start session",
@@ -103,9 +95,6 @@ angular.module('auction').controller('AuctionController', [
     $rootScope.DAYS_IN_YEAR = 365;
     $rootScope.NPV_CALCULATION_DURATION = 20;
     growlMessages.initDirective(0, 10);
-    dataLayer.push({
-      "tenderId": AuctionConfig.auction_doc_id
-    });
     if (($translate.storage().get($translate.storageKey()) === "undefined") || ($translate.storage().get($translate.storageKey()) === undefined)) {
       $translate.use(AuctionConfig.default_lang);
       $rootScope.lang = AuctionConfig.default_lang;
